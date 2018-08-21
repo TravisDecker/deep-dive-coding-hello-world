0605fd30a21da688a8941b82c42ca4db8c74e5b3
travis <travis@172.20.20.20>
2018-08-20 19:24:11 -0600

    Final save files

--- src/edu/cnm/deepdive/helloworld/HelloWorld.java
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -5,15 +5,16 @@ package edu.cnm.deepdive.helloworld;
 
 /**
  * @author travis
- *
+ * a simple class that prints "Hello World".
  */
 public class HelloWorld {
 
 	/**
+	 * prints "Hello world" to standerd output.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello world");
 
 	}
 
